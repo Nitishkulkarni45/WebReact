2ea6f1b4d35ed6fb6a1627f2d897d54370749d65
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2023-04-21 03:07:20 +0530

    MOdified

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import "./SignUp.css";
-import Navbar from './Navbar';
-import Hero from '../Routes/Hero';
+// import Navbar from './Navbar';
+// import Hero from '../Routes/Hero'; 
 import SignUpform from './SignUpform';
 
 function SignUp  ()  {
   return (
     <>
-    <Navbar 
+    {/* <Navbar 
     classBoot1="nav-link navb-linker Boot"
     classBoot2="nav-link navb-linker Boot"
     classBoot3="nav-link navb-linker Boot"
     classBoot4="nav-link navb-linker Boot"
     classBoot5="nav-link active navb-linker Boot2"
-    />
+    /> */}
     <SignUpform />
     </>
     
--- src/components/SignUpform.js
+++ src/components/SignUpform.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import "SignUp.css";
+import "./SignUp.css";
+import img1 from "./../assets/Cheers.png";
 
-const SignUpform = () => {
+function SignUpform() {
   return (
-    <div>SignUpform</div>
+    <div className='SignUpFormContainer'>
+      <div className="immmm"></div>
+      <img src={img1} alt="fdasc" />
+      <div className="originform">
+        <input type="text" placeholder='First Name:' />
+        <input type="text" placeholder='Last Name:' />
+        <input type="email" placeholder='Email:' />
+        <input type="password" placeholder='Password' />
+        <div className="submitt">
+        <button>Sign Up</button>
+        <a href="/">Already have an account?</a>
+        </div>
+       
+      </div>
+    </div>
   )
 }
 
