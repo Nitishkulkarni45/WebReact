cf50a8baf5d15d3c8968131e12db3713fe439354
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2023-05-21 21:16:23 +0530

    navbar added->Hotels page

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import './Hotels.css';
+import '../components/Hotels.css';
+import Navbar from './Navbar';
 
 const Hotels = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Navbar.css";
 import {Link} from "react-router-dom";
-import img1 from "./../assets/Cheers.png";
+
 
 
 const Navbar = (props) => {
